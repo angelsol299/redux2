fee18ae83667a8ef9fad972b62f5fb81955d9580
angelsol299 <angelsol299@hotmail.com>
2019-03-22 20:07:46 +0100

    more config and first tests

--- newtesting3/src/App.js
+++ newtesting3/src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
 import "./App.css";
 import Header from "./components/Header";
+import Headline from "./components/Headline/Headline";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
+        <Header data-test="headerComponent" />
+        <section className="main">
+          <Headline
+            header="Posts"
+            desc="Click the button to render the posts"
+          />
+        </section>
       </div>
     );
   }
